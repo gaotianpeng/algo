006c75ce65b151679966b0774f7a347af3154cf6
gaotianpeng <gtpsydm@gmail.com>
2021-04-16 20:30:30 +0800

    sum of factorial in java

---
+++ Code05_SumOfFactorial.java
@@ -0,0 +1,18 @@
+package AlgoNew;
+
+public class Code05_SumOfFactorial {
+    static long factorial(int N) {
+        long ans = 0;
+        long cur = 1;
+        for (int i = 1; i <= N; ++i) {
+            cur = cur * i;
+            ans += cur;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
+}
