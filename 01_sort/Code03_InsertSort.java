3c0603cad749fdf5114180dacb2437eda6af69db
gaotianpeng <gtpsydm@gmail.com>
2021-11-07 23:15:43 +0800

    find value in sorted array

---
+++ 01_sort/Code03_InsertSort.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+
+public class Code03_InsertSort {
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        /*
+            0 ~ 1 上有序
+            0 ~ 2 上有序
+            ..
+            0 ~ N-1 上有序
+        */
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--) {
+                swap(arr, j, j+1);
+            }
+        }
+    }
+
+    /*
+        for test
+    */
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+
+        int[] ret = new int[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+            ret[i] = arr[i];
+        }
+
+        return ret;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.println(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; ++i) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            insertSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+
+        System.out.println(succeed ? "Nice!": "Failed!");
+    }
+}
---
+++ 02_binary/Code01_BSExist.java
@@ -0,0 +1,67 @@
+package binary;
+
+import java.util.Arrays;
+
+public class Code01_BSExist {
+
+    public static boolean exist(int[] sortedArr, int num) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
+
+        int left = 0;
+        int right = sortedArr.length - 1;
+        int mid = 0;
+
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+            if (sortedArr[mid] == num) {
+                return true;
+            } else if (sortedArr[mid] > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return sortedArr[left] == num;
+    }
+
+    /*
+        for test
+    */
+    public static boolean test(int[] sortedArr, int num) {
+        for (int cur: sortedArr) {
+            if (cur == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 30;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (test(arr, value) != exist(arr, value)) {
+                succeed = false;
+                break;
+            }
+        }
+
+        System.out.println(succeed ? "Success" : "Failed");
+    }
+}
