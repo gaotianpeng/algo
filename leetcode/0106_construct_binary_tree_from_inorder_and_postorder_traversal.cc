0169dd4d3e15cc986bd54f7e4693d2a4146d02e7
gaotianpeng <gtpsydm@gmail.com>
2021-12-26 20:44:35 +0800

    daily practise

---
+++ leetcode/0106_construct_binary_tree_from_inorder_and_postorder_traversal.cc
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+/*
+    106 从中序与后序遍历序列构造二叉树
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+    }
+};
