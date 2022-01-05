b69d397ef799f23b9eeae710aea5053be662ed71
gaotianpeng <gtpsydm@gmail.com>
2022-01-05 11:23:14 +0800

    daily practise

--- 01_sort/Code10_SmallSum.java
+++ 01_sort/Code10_SmallSum.java
@@ -1,58 +1,46 @@
 package sort;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+/*
+    在一个数组中，一个数左边比它小的数的总和，叫数的小和，所有数的小和累加起来，叫数组小和。求数组小和。
+    例子： [1,3,4,2,5]
+    1左边比1小的数：没有
+    3左边比3小的数：1
+    4左边比4小的数：1、3
+    2左边比2小的数：1
+    5左边比5小的数：1、3、4、 2
+    所以数组的小和为1+1+3+1+1+3+4+2=16
+ */
 
-public class Code09_MergeSort {
+public class Code10_SmallSum {
 
-    public static void mergeSort(int[] arr) {
+    public static int smallSum(int[] arr) {
         if (arr == null || arr.length < 2) {
-            return;
+            return 0;
         }
-        process(arr, 0, arr.length  - 1);
+        return process(arr, 0, arr.length - 1);
     }
 
-    public static void process(int[] arr, int left, int right) {
+    public static int process(int[] arr, int left, int right) {
         if (left == right) {
-            return;
+            return 0;
         }
 
-        int mid = left +((right - left) >> 1);
-        process(arr, left, mid);
-        process(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        int mid = left + ((right - left)>>1);
+        return process(arr, left, mid)
+                + process(arr, mid + 1, right)
+                + merge(arr, left, mid, right);
     }
 
-    public static void mergeSort2(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-
-        int arr_len = arr.length;
-        int merge_step = 1;
-        while (merge_step < arr_len) {
-            int left = 0;
-            while (left < arr_len) {
-                if (merge_step > arr_len - left) {
-                    break;
-                }
-
-                int mid = left + merge_step - 1;
-                int right = mid + Math.min(merge_step, arr_len - mid - 1);
-                merge(arr, left, mid, right);
-                left = right + 1;
-            }
-            merge_step <<= 1;
-        }
-    }
-
-    public static void merge(int[] arr, int left, int mid, int right) {
+    public static int merge(int[] arr, int left, int mid, int right) {
         int[] helper = new int[right - left + 1];
         int pos1 = left;
         int pos2 = mid + 1;
+
         int index = 0;
+        int result = 0;
         while (pos1 <= mid && pos2 <= right) {
-            if (arr[pos1] <= arr[pos2]) {
+            if (arr[pos1] < arr[pos2]) {
+                result += ((right - pos2 + 1)*arr[pos1]);
                 helper[index++] = arr[pos1++];
             } else {
                 helper[index++] = arr[pos2++];
@@ -66,17 +54,32 @@ public class Code09_MergeSort {
             helper[index++] = arr[pos2++];
         }
 
-        for (int i = 0; i < helper.length; i++) {
+        for (int i = 0; i < helper.length; ++i) {
             arr[left + i] = helper[i];
         }
+
+        return result;
     }
 
 
     /*
         for test
-    */
-    public static void test(int[] arr) {
-        Arrays.sort(arr);
+     */
+    public static int test(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+
+        int small_sum = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    small_sum += arr[j];
+                }
+            }
+        }
+
+        return small_sum;
     }
 
     public static int[] generateRandomArray(int maxSize, int maxValue) {
@@ -86,6 +89,12 @@ public class Code09_MergeSort {
         }
 
         return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static int[] copyArray(int[] arr) {
@@ -129,7 +138,7 @@ public class Code09_MergeSort {
         }
 
         for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
+            System.out.println(arr[i] + " ");
         }
 
         System.out.println();
@@ -137,33 +146,21 @@ public class Code09_MergeSort {
 
     public static void main(String[] args) {
         System.out.println("test start...");
-
-        int test_times = 1000000;
-        int max_val = 40;
-        int max_len = 50;
+        int test_times = 10000000;
+        int max_value = 30;
+        int max_len = 20;
         boolean success = true;
-        for (int i = 0; i < test_times; ++i) {
-            int[] arr1 = generateRandomArray(max_len, max_val);
+        for (int i = 0; i < test_times; i++) {
+            int[] arr1 = generateRandomArray(max_len, max_value);
             int[] arr2 = copyArray(arr1);
-            int[] arr3 = copyArray(arr1);
-            mergeSort(arr1);
-            mergeSort2(arr2);
-            test(arr3);
-            if (!isEqual(arr1, arr3)) {
-                success = false;
+            if (smallSum(arr1) != test(arr2)) {
                 printArray(arr1);
-                printArray(arr3);
-                break;
-            }
-            if (!isEqual(arr2, arr3)) {
                 success = false;
-                printArray(arr2);
-                printArray(arr3);
                 break;
             }
         }
 
-        System.out.println(success ? "success": "failed");
+        System.out.println(success? "success" : "failed");
         System.out.println("test end");
     }
 }
---
+++ 01_sort/Code11_QuickSort.java
@@ -0,0 +1,139 @@
+package sort;
+
+import java.util.Arrays;
+
+public class Code11_QuickSort {
+    public static void swap(int[] arr, int i, int j) {
+//        arr[i] = arr[i] ^ arr[j];
+//        arr[j] = arr[i] ^ arr[j];
+//        arr[i] = arr[i] ^ arr[j];
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] =  temp;
+    }
+
+    // 在arr[left...right]上，以arr[right]位置的数做划分值
+    public static int partition(int[] arr, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        if (left == right) {
+            return left;
+        }
+
+        int less_equal = left - 1;
+        int index = left;
+        while (index < right) {
+            if (arr[index] <= arr[right]) {
+                swap(arr, index, ++less_equal);
+            }
+            index++;
+        }
+        swap(arr, ++less_equal, right);
+
+        return less_equal;
+    }
+
+    public static void process(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int mid = partition(arr, left, right);
+        process(arr, left, mid - 1);
+        process(arr, left + 1, right);
+    }
+
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    /*
+        for test
+    */
+    public static void test(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+
+        int[] ret = new int[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+            ret[i] = arr[i];
+        }
+
+        return ret;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test start...");
+        int test_times = 100000;
+        int max_val = 50;
+        int max_len = 10;
+        boolean success = true;
+
+        for (int i = 0; i < test_times; i++) {
+            int[] arr1 = generateRandomArray(max_len, max_val);
+            int[] arr2 = copyArray(arr1);
+            quickSort(arr1);
+            test(arr2);
+            if (!isEqual(arr1, arr2)) {
+                printArray(arr1);
+                success = false;
+                break;
+            }
+        }
+        System.out.println(success ? "success": "failed");
+        System.out.println("test end");
+    }
+}
