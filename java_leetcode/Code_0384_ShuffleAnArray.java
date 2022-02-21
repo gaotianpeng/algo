d3c2413b2d5bbefacdd64a6549891727a93f378d
gaotianpeng <gtpsydm@gmail.com>
2022-02-21 10:30:37 +0800

    daily practise

--- java_leetcode/Code_0384_ShuffleAnArray.java
+++ java_leetcode/Code_0384_ShuffleAnArray.java
@@ -1,25 +1,24 @@
 package leetcode;
 /*
     384 打乱数组
+        给你一个整数数组nums，设计算法来打乱一个没有重复元素的数组。打乱后，数组的所有排列应该是等可能的。
+        实现 Solution class:
+            Solution(int[] nums) 使用整数数组 nums 初始化对象
+            int[] reset() 重设数组到它的初始状态并返回
+            int[] shuffle() 返回数组随机打乱后的结果
 
-    给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。打乱后，数组的所有排列应该是等可能的。
-    实现 Solution class:
-        Solution(int[] nums) 使用整数数组 nums 初始化对象
-        int[] reset() 重设数组到它的初始状态并返回
-        int[] shuffle() 返回数组随机打乱后的结果
+        示例
+            输入
+                ["Solution", "shuffle", "reset", "shuffle"]
+                [[[1, 2, 3]], [], [], []]
+            输出
+                [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
 
-    示例
-        输入
-        ["Solution", "shuffle", "reset", "shuffle"]
-        [[[1, 2, 3]], [], [], []]
-        输出
-        [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
-
-        解释
-        Solution solution = new Solution([1, 2, 3]);
-        solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 1, 2]
-        solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
-        solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
+            解释
+                Solution solution = new Solution([1, 2, 3]);
+                solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 1, 2]
+                solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
+                solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
 
  */
 public class Code_0384_ShuffleAnArray {
@@ -42,10 +41,10 @@ public class Code_0384_ShuffleAnArray {
         }
 
         public int[] shuffle() {
-            for (int i = n - 1; i >= 0; i--) {
-                int r = (int)(Math.random() * (i+1));
-                int tmp = shuffle[r];
-                shuffle[r] = shuffle[i];
+            for (int i = n -1; i >= 0; i--) {
+                int random_pos = (int)(Math.random() * (i + 1));
+                int tmp = shuffle[random_pos];
+                shuffle[random_pos] = shuffle[i];
                 shuffle[i] = tmp;
             }
 
