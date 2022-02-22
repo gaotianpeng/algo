241685ef4d7a9b2fd3905e5882fa9f9ebe979a01
gaotianpeng <gtpsydm@gmail.com>
2022-02-22 09:35:19 +0800

    daily practise

---
+++ shuati/Code_001_CordCoverMaxPoint.java
@@ -0,0 +1,114 @@
+package shuati;
+/*
+    给定一个有序数组arr，代表坐落在X轴上的点
+    给定一个正数K，代表绳子的长度
+    返回绳子最多压中几个点？
+    即使绳子边缘处盖住点也算盖住
+ */
+
+import java.util.Arrays;
+
+public class Code_001_CordCoverMaxPoint {
+
+    public static int maxPoint1(int[] arr, int len) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int max = 1;
+        for (int i = 0; i < arr.length; i++) {
+            int nearest = nearestIndex(arr, i, arr[i] - len);
+            max = Math.max(max, i - nearest + 1);
+        }
+
+        return max;
+    }
+
+    public static int nearestIndex(int[] arr, int right, int val) {
+        int left = 0;
+        int index = right;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] >= val) {
+                index = mid;
+                right = mid - 1;
+            } else {
+                left = mid +1;
+            }
+        }
+        return index;
+    }
+
+
+    public static int maxPoint2(int[] arr, int len) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        int n = arr.length;
+        while (left < n) {
+            while (right < n && arr[right] - arr[left] <= len) {
+                right++;
+            }
+            max = Math.max(max, right - (left++));
+        }
+        return max;
+    }
+
+    /*
+        for test
+     */
+    public static int test(int[] arr, int len) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int pre = i - 1;
+            while (pre >= 0 && arr[i] - arr[pre] <= len) {
+                --pre;
+            }
+            max = Math.max(max, i - pre);
+        }
+
+        return max;
+    }
+
+    public static int[] generateArray(int len, int max) {
+        int[] ans = new int[(int)(Math.random() * len) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int)(Math.random() * max);
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test start...");
+        int len = 100;
+        int max = 1000;
+        int test_times = 1000000;
+        boolean success = true;
+        for (int i = 0; i < test_times; i++) {
+            int line_len = (int)(Math.random() * max);
+            int[] arr = generateArray(len, max);
+            int ans1 = maxPoint1(arr, line_len);
+            int ans2 = maxPoint2(arr, line_len);
+            int ans3 = test(arr, line_len);
+            if (ans1 != ans3 || ans2 != ans3) {
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                success = false;
+                break;
+            }
+        }
+
+        System.out.println(success ? "success" : "failed");
+        System.out.println("test end...");
+    }
+}
