35f6dcc6b796f1b3b799083531c13baaa635369a
gaotianpeng <gtpsydm@gmail.com>
2022-03-14 17:31:39 +0800

    daily practise

--- tixi/daily10/Code01_FindFirstIntersectionNode.java
+++ tixi/daily10/Code01_FindFirstIntersectionNode.java
@@ -6,27 +6,38 @@ package tixi.daily10;
         如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度 请达到O(1)。
  */
 public class Code01_FindFirstIntersectionNode {
-    public static class Node {
+    public static class ListNode {
         public int value;
-        public Node next;
+        public ListNode next;
 
-        public Node(int val) {
+        public ListNode(int val) {
             value = val;
             next = null;
         }
     }
-
-    public static Node getIntersectionNode(Node head1, Node head2) {
+    public static ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        ListNode loop1 = getLoopListNode(head1);
+        ListNode loop2 = getLoopListNode(head2);
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1, head2);
+        }
+        if (loop1 != null && loop2 != null) {
+            return bothLoop(head1, loop1, head2, loop2);
+        }
         return null;
     }
 
-    public static Node getLoopNode(Node head) {
-        if (head == null || head.next == null || head.next.next == null)  {
+    // 找到链表第一个入环节点，如果无环，返回null
+    public static ListNode getLoopListNode(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
             return null;
         }
-
-        Node slow = head.next;
-        Node fast = head.next.next;
+        // n1 慢  n2 快
+        ListNode slow = head.next; // n1 -> slow
+        ListNode fast = head.next.next; // n2 -> fast
         while (slow != fast) {
             if (fast.next == null || fast.next.next == null) {
                 return null;
@@ -34,13 +45,87 @@ public class Code01_FindFirstIntersectionNode {
             fast = fast.next.next;
             slow = slow.next;
         }
-
-        fast = head;
-        while (slow != head) {
+        // slow fast  相遇
+        fast = head; // n2 -> walk again from head
+        while (slow != fast) {
             slow = slow.next;
             fast = fast.next;
         }
         return slow;
+    }
+
+    // 如果两个链表都无环，返回第一个相交节点，如果不想交，返回null
+    public static ListNode noLoop(ListNode head1, ListNode head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        ListNode cur1 = head1;
+        ListNode cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        if (cur1 != cur2) {
+            return null;
+        }
+        // n  :  链表1长度减去链表2长度的值
+        cur1 = n > 0 ? head1 : head2; // 谁长，谁的头变成cur1
+        cur2 = cur1 == head1 ? head2 : head1; // 谁短，谁的头变成cur2
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    // 两个有环链表，返回第一个相交节点，如果不想交返回null
+    public static ListNode bothLoop(ListNode head1, ListNode loop1, ListNode head2, ListNode loop2) {
+        ListNode cur1 = null;
+        ListNode cur2 = null;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+            n = Math.abs(n);
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
     }
 
     public static void main(String[] args) {
@@ -48,10 +133,8 @@ public class Code01_FindFirstIntersectionNode {
         int test_times = 10000;
         boolean success = true;
         for (int i = 0; i < test_times; i++) {
-
         }
         System.out.println(success ? "success" : "failed");
         System.out.println("test end");
     }
-
 }
