84d675fde053edebdcb70d55a3f01d0d3f648fae
gaotianpeng <gtpsydm@gmail.com>
2022-03-15 10:29:29 +0800

    daily practise

--- tixi/daily10/Code01_FindFirstIntersectionNode.java
+++ tixi/daily10/Code01_FindFirstIntersectionNode.java
@@ -1,9 +1,10 @@
 package tixi.daily10;
 /*
-    给定两个可能有环也可能无环的单链表，头节点head1和head2。请实现一个函数，
-    如果两个链表相交，请返回相交的 第一个节点。如果不相交，返回null
-    【要求】
-        如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度 请达到O(1)。
+    leetcode 160 相交链表
+        给定两个可能有环也可能无环的单链表，头节点head1和head2。请实现一个函数，
+        如果两个链表相交，请返回相交的 第一个节点。如果不相交，返回null
+        【要求】
+            如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度 请达到O(1)。
  */
 public class Code01_FindFirstIntersectionNode {
     public static class ListNode {
@@ -15,18 +16,23 @@ public class Code01_FindFirstIntersectionNode {
             next = null;
         }
     }
+
     public static ListNode getIntersectionNode(ListNode head1, ListNode head2) {
         if (head1 == null || head2 == null) {
             return null;
         }
+
         ListNode loop1 = getLoopListNode(head1);
         ListNode loop2 = getLoopListNode(head2);
+
         if (loop1 == null && loop2 == null) {
             return noLoop(head1, head2);
         }
+
         if (loop1 != null && loop2 != null) {
             return bothLoop(head1, loop1, head2, loop2);
         }
+
         return null;
     }
 
@@ -35,23 +41,24 @@ public class Code01_FindFirstIntersectionNode {
         if (head == null || head.next == null || head.next.next == null) {
             return null;
         }
-        // n1 慢  n2 快
-        ListNode slow = head.next; // n1 -> slow
-        ListNode fast = head.next.next; // n2 -> fast
+
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
         while (slow != fast) {
             if (fast.next == null || fast.next.next == null) {
                 return null;
             }
+            slow = slow.next;
             fast = fast.next.next;
-            slow = slow.next;
         }
-        // slow fast  相遇
-        fast = head; // n2 -> walk again from head
-        while (slow != fast) {
-            slow = slow.next;
+
+        fast = head;
+        while (fast != slow) {
             fast = fast.next;
+            slow = slow.next;
         }
-        return slow;
+
+        return fast;
     }
 
     // 如果两个链表都无环，返回第一个相交节点，如果不想交，返回null
@@ -59,36 +66,41 @@ public class Code01_FindFirstIntersectionNode {
         if (head1 == null || head2 == null) {
             return null;
         }
-        ListNode cur1 = head1;
-        ListNode cur2 = head2;
+
         int n = 0;
-        while (cur1.next != null) {
+        ListNode cur1 = head1;
+        while (cur1 != null) {
             n++;
             cur1 = cur1.next;
         }
-        while (cur2.next != null) {
+
+        ListNode cur2 = head2;
+        while (cur2 != null) {
             n--;
             cur2 = cur2.next;
         }
+
         if (cur1 != cur2) {
             return null;
         }
-        // n  :  链表1长度减去链表2长度的值
-        cur1 = n > 0 ? head1 : head2; // 谁长，谁的头变成cur1
-        cur2 = cur1 == head1 ? head2 : head1; // 谁短，谁的头变成cur2
+
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
         n = Math.abs(n);
         while (n != 0) {
             n--;
             cur1 = cur1.next;
         }
+
         while (cur1 != cur2) {
             cur1 = cur1.next;
             cur2 = cur2.next;
         }
+
         return cur1;
     }
 
-    // 两个有环链表，返回第一个相交节点，如果不想交返回null
+    // 两个有环链表，返回第一个相交节点，如果不相交返回null
     public static ListNode bothLoop(ListNode head1, ListNode loop1, ListNode head2, ListNode loop2) {
         ListNode cur1 = null;
         ListNode cur2 = null;
@@ -100,7 +112,7 @@ public class Code01_FindFirstIntersectionNode {
                 n++;
                 cur1 = cur1.next;
             }
-            while (cur2 != loop2) {
+            while (cur2 != loop1) {
                 n--;
                 cur2 = cur2.next;
             }
@@ -108,8 +120,8 @@ public class Code01_FindFirstIntersectionNode {
             cur2 = cur1 == head1 ? head2 : head1;
             n = Math.abs(n);
             while (n != 0) {
-                n--;
                 cur1 = cur1.next;
+                n--;
             }
             while (cur1 != cur2) {
                 cur1 = cur1.next;
@@ -120,21 +132,11 @@ public class Code01_FindFirstIntersectionNode {
             cur1 = loop1.next;
             while (cur1 != loop1) {
                 if (cur1 == loop2) {
-                    return loop1;
+                    return loop2;
                 }
                 cur1 = cur1.next;
             }
             return null;
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println("test start...");
-        int test_times = 10000;
-        boolean success = true;
-        for (int i = 0; i < test_times; i++) {
-        }
-        System.out.println(success ? "success" : "failed");
-        System.out.println("test end");
     }
 }
