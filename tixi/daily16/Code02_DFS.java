1dd045a4c3b149b5f2b872f549d255aec41f0bef
gaotianpeng <gtpsydm@gmail.com>
2022-04-02 10:07:48 +0800

    daily practise

--- tixi/daily16/Code02_DFS.java
+++ tixi/daily16/Code02_DFS.java
@@ -5,34 +5,6 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class Code02_DFS {
-    public static  class Edge {
-        public int weight;
-        public Node from;
-        public Node to;
-
-        public Edge(int weight, Node from, Node to) {
-            this.weight = weight;
-            this.from = from;
-            this.to = to;
-        }
-    }
-
-    public static class Node {
-        public int value;
-        public int in;
-        public int out;
-        public ArrayList<Node> nexts;
-        public ArrayList<Edge> edges;
-
-        public Node(int value) {
-            this.value = value;
-            in = 0;
-            out = 0;
-            nexts = new ArrayList<>();
-            edges = new ArrayList<>();
-        }
-    }
-
     static void dfs(Node node) {
         if (node == null) {
             return;
