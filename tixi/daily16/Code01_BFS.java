a08dfedf209f423ec4ff672b3a676838f4dcac03
gaotianpeng <gtpsydm@gmail.com>
2022-04-04 17:20:24 +0800

    daily practise

--- tixi/daily16/Code01_BFS.java
+++ tixi/daily16/Code01_BFS.java
@@ -1,30 +1,30 @@
 package tixi.daily16;
 
-import java.util.*;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Code01_BFS {
     // 从node出发, 进行宽度优先遍历
-    public static List<Integer> bfs(Node start) {
+    public static void bfs(Node start) {
         if (start == null) {
-            return null;
+            return;
         }
 
-        List<Integer> ans = new ArrayList<>();
         Queue<Node> queue = new LinkedList<>();
         HashSet<Node> set = new HashSet<>();
         queue.add(start);
         set.add(start);
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
-            ans.add(cur.value);
-            for (Node next : cur.nexts) {
-                if (!set.contains(next)) {
-                    set.add(next);
-                    queue.add(next);
+            System.out.println(cur.value);
+            for (Node node: cur.nexts) {
+                if (!set.contains(node)) {
+                    set.add(node);
+                    queue.add(node);
                 }
             }
         }
-
-        return ans;
     }
 }
