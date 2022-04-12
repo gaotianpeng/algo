ba7f6707a1c07958796318583ee4589157228cf8
gaotianpeng <gtpsydm@gmail.com>
2022-04-12 17:18:31 +0800

    daily practise

--- tixi/daily17/Code03_PrintAllPermutations.java
+++ tixi/daily17/Code03_PrintAllPermutations.java
@@ -1,6 +1,9 @@
 package tixi.daily17;
 
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_sv;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -38,6 +41,69 @@ public class Code03_PrintAllPermutations {
         }
     }
 
+    public static List<String> permutation2(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            return ans;
+        }
+
+        char[] str = s.toCharArray();
+        process2(str, 0, ans);
+        return ans;
+    }
+
+    private static void process2(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+        } else {
+            for (int i = index; i < str.length; i++) {
+                swap(str, i, index);
+                process2(str, index + 1, ans);
+                swap(str, index, i);
+            }
+        }
+    }
+
+    private static void swap(char[] str, int i, int j) {
+        char ch = str[i];
+        str[i] = str[j];
+        str[j] = ch;
+    }
+
+    /*
+        for test
+     */
+    public static boolean isEqual(List<String> list1, List<String> list2) {
+        if (list1 == null && list2 == null) {
+            return true;
+        }
+
+        if (list1 == null && list2 != null) {
+            return false;
+        }
+
+        if (list1 != null && list2 == null) {
+            return false;
+        }
+
+        if (list1.size() != list2.size()) {
+            return false;
+        }
+
+        String[] strs1 = new String[list1.size()];
+        String[] strs2 = new String[list2.size()];
+        list1.toArray(strs1);
+        list2.toArray(strs2);
+        Arrays.sort(strs1);
+        Arrays.sort(strs2);
+
+        for (int i = 0; i < strs1.length; i++) {
+            if (!strs1[i].equals(strs2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void print(List<String> strs) {
         if (strs == null) {
@@ -45,14 +111,39 @@ public class Code03_PrintAllPermutations {
         }
 
         for (String cur: strs) {
-            System.out.println(cur);
+            System.out.print(cur + " ");
         }
+        System.out.println();
+    }
+
+    public static String generateRandomString(int max_len) {
+        int len = (int)(Math.random() * (max_len  + 1));
+        char[] str = new char[len];
+        for (int i = 0; i < len; i++) {
+            str[i] = (char)(97 + (int)(Math.random()*26));
+        }
+
+        return String.valueOf(str);
     }
 
     public static void main(String[] args) {
-        System.out.println("test start...");
-        List<String> ans = permutation("abc");
-        print(ans);
+        System.out.println("test start....");
+        int test_times = 100000;
+        int max_len = 6;
+        boolean success = true;
+        for (int i = 0; i < test_times; i++) {
+            String str = generateRandomString(max_len);
+            List<String> ans1 = permutation(str);
+            List<String> ans2 = permutation2(str);
+            if (!isEqual(ans1, ans2)) {
+                print(ans1);
+                print(ans2);
+                success = false;
+                break;
+            }
+        }
+
+        System.out.println(success ? "success" : "failed");
         System.out.println("test end");
     }
 }
