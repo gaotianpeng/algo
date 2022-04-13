fddb195211b2f9762b04921d8b3cbffd06d56105
gaotianpeng <gtpsydm@gmail.com>
2022-04-13 08:41:41 +0800

    daily practise

--- tixi/daily17/Code04_PrintAllPermutations2.java
+++ tixi/daily17/Code04_PrintAllPermutations2.java
@@ -2,12 +2,14 @@ package tixi.daily17;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /*
-    打印一个字符串的全部排列，要求不要出现重复的排列
+    打印一个字符串的全部排列
  */
-public class Code03_PrintAllPermutations {
+public class Code04_PrintAllPermutations2 {
+
     public static List<String> permutation(String s) {
         List<String> ans = new ArrayList<>();
         if (s == null || s.length() == 0) {
@@ -15,57 +17,93 @@ public class Code03_PrintAllPermutations {
         }
 
         char[] str = s.toCharArray();
-        ArrayList<Character> rest = new ArrayList<>();
-        for (char ch: str) {
-            rest.add(ch);
+        HashSet<String> set = new HashSet<>();
+        process1(str, 0, set);
+        for (String cur: set) {
+            ans.add(cur);
         }
-        String path = "";
-        process(rest, path, ans);
+
         return ans;
     }
 
-    private static void process(ArrayList<Character> rest, String path, List<String> ans) {
-        if (rest.isEmpty()) {
-            ans.add(path);
-        } else {
-            int n = rest.size();
-            for (int i = 0; i < n; i++) {
-                char cur = rest.get(i);
-                rest.remove(i);
-                process(rest, path + cur, ans);
-                rest.add(i, cur);
-            }
-        }
-    }
-
-    public static List<String> permutation2(String s) {
-        List<String> ans = new ArrayList<>();
-        if (s == null || s.length() == 0) {
-            return ans;
-        }
-
-        char[] str = s.toCharArray();
-        process2(str, 0, ans);
-        return ans;
-    }
-
-    private static void process2(char[] str, int index, List<String> ans) {
+    private static void process1(char[] str, int index, HashSet<String> ans) {
         if (index == str.length) {
             ans.add(String.valueOf(str));
         } else {
             for (int i = index; i < str.length; i++) {
                 swap(str, i, index);
-                process2(str, index + 1, ans);
+                process1(str, index + 1, ans);
                 swap(str, index, i);
             }
         }
     }
+
+    public static List<String> permutation2(String s) {
+        ArrayList<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            return ans;
+        }
+
+        ArrayList<Character> rest = new ArrayList<>();
+        char[] str = s.toCharArray();
+        for (char ch: str) {
+            rest.add(ch);
+        }
+        HashSet<String> set = new HashSet<>();
+        String path = "";
+        process2(rest, path, set);
+        for (String cur: set) {
+            ans.add(cur);
+        }
+        return ans;
+    }
+
+    private static void process2(ArrayList<Character> rest, String path, HashSet<String> ans) {
+        if (rest.isEmpty()) {
+            ans.add(path);
+        } else {
+            for (int i = 0; i < rest.size(); i++) {
+                char ch = rest.get(i);
+                rest.remove(i);
+                process2(rest, path + ch, ans);
+                rest.add(i, ch);
+            }
+        }
+    }
+
 
     private static void swap(char[] str, int i, int j) {
         char ch = str[i];
         str[i] = str[j];
         str[j] = ch;
     }
+    public static List<String> permutation3(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            return ans;
+        }
+
+        char[] str = s.toCharArray();
+        process3(str, 0, ans);
+        return ans;
+    }
+
+    private static void process3(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+        } else {
+            boolean[] is_visited = new boolean[256];
+            for (int i = index; i < str.length; i++) {
+                if (!is_visited[str[i]]) {
+                    is_visited[str[i]] = true;
+                    swap(str, index, i);
+                    process3(str, index + 1, ans);
+                    swap(str, i, index);
+                }
+            }
+        }
+    }
+
     /*
         for test
      */
@@ -131,9 +169,18 @@ public class Code03_PrintAllPermutations {
             String str = generateRandomString(max_len);
             List<String> ans1 = permutation(str);
             List<String> ans2 = permutation2(str);
+            List<String> ans3 = permutation3(str);
+
             if (!isEqual(ans1, ans2)) {
                 print(ans1);
                 print(ans2);
+                success = false;
+                break;
+            }
+
+            if (!isEqual(ans1, ans3)) {
+                print(ans1);
+                print(ans3);
                 success = false;
                 break;
             }
