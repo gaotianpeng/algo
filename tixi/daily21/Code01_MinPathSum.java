99bac9261da4229033afcc2dafc8318d80c0f0c2
gaotianpeng <gtpsydm@gmail.com>
2022-05-03 09:04:25 +0800

    daily practise

--- tixi/daily21/Code01_MinPathSum.java
+++ tixi/daily21/Code01_MinPathSum.java
@@ -24,10 +24,53 @@ public class Code01_MinPathSum {
 
         for (int i = 1; i < row; i++) {
             for (int j = 1; j < col; j++) {
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1] + m[i][j]);
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + m[i][j];
             }
         }
 
         return dp[row - 1][col - 1];
+    }
+
+    public static int minPathSum2(int[][] m) {
+        if (m == null || m.length == 0 || m[0] == null || m.length == 0) {
+            return 0;
+        }
+
+        int row = m.length;
+        int col = m[0].length;
+        int[] dp = new int[col];
+        dp[0] = m[0][0];
+        for (int j = 1; j < col; j++) {
+            dp[j] = dp[j-1] + m[0][j];
+        }
+
+        for (int i = 1; i < row; i++) {
+            dp[0] += m[i][0];
+            for (int j = 1; j < col; j++) {
+                dp[j] = Math.min(dp[j-1], dp[j])+ m[i][j];
+            }
+        }
+
+        return dp[col - 1];
+    }
+    public static int[][] generateRandomMatrix(int rowSize, int colSize) {
+        if (rowSize < 0 || colSize < 0) {
+            return null;
+        }
+        int[][] result = new int[rowSize][colSize];
+        for (int i = 0; i != result.length; i++) {
+            for (int j = 0; j != result[0].length; j++) {
+                result[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rowSize = 10;
+        int colSize = 10;
+        int[][] m = generateRandomMatrix(rowSize, colSize);
+        System.out.println(minPathSum(m));
+        System.out.println(minPathSum2(m));
     }
 }
---
+++ tixi/daily21/Code02_CoinsWayEveryPaperDifferent.java
@@ -0,0 +1,28 @@
+package tixi.daily21;
+/*
+    arr是货币数组，其中的值都是正数。再给定一个正数aim。
+    每个值都认为是一张货币，
+    即便是值相同的货币也认为每一张都是不同的，
+    返回组成aim的方法数
+    例如：arr = {1,1,1}，aim = 2
+    第0个和第1个能组成2，第1个和第2个能组成2，第0个和第2个能组成2
+    一共就3种方法，所以返回3
+ */
+public class Code02_CoinsWayEveryPaperDifferent {
+    public static int coinWays(int[] arr, int aim) {
+        return process(arr, 0, aim);
+    }
+
+    public static int process(int[] arr, int index, int rest) {
+        if (rest < 0) {
+            return 0;
+        }
+
+        if (index == arr.length) {
+            return rest == 0 ? 1 : 0;
+        } else {
+            return process(arr, index + 1, rest)
+                + process(arr, index + 1, rest - arr[index]);
+        }
+    }
+}
