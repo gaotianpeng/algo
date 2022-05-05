390aa15471dffc26b6b1ca47cbbd9dc06ed7606d
gaotianpeng <gtpsydm@gmail.com>
2022-05-05 10:03:56 +0800

    daily practise

--- tixi/Code02_CoinsWayEveryPaperDifferent.java
+++ tixi/Code02_CoinsWayEveryPaperDifferent.java
@@ -40,4 +40,45 @@ public class Code02_CoinsWayEveryPaperDifferent {
         }
         return dp[0][aim];
     }
+
+    // 为了测试
+    public static int[] randomArray(int maxLen, int maxValue) {
+        int N = (int) (Math.random() * maxLen);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = (int) (Math.random() * maxValue) + 1;
+        }
+        return arr;
+    }
+
+    // 为了测试
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int maxLen = 20;
+        int maxValue = 30;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = randomArray(maxLen, maxValue);
+            int aim = (int) (Math.random() * maxValue);
+            int ans1 = coinWays(arr, aim);
+            int ans2 = dp(arr, aim);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println(aim);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
