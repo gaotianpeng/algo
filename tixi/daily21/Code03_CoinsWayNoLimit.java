259d88d04831b8dadb456f11975b74e030b34da1
gaotianpeng <gtpsydm@gmail.com>
2022-05-07 21:55:09 +0800

    daily practise

--- tixi/daily21/Code03_CoinsWayNoLimit.java
+++ tixi/daily21/Code03_CoinsWayNoLimit.java
@@ -26,4 +26,23 @@ public class Code03_CoinsWayNoLimit {
         }
         return ways;
     }
+
+    public static int dp1(int[] arr, int aim) {
+		if (arr == null || arr.length == 0 || aim < 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int[][] dp = new int[N + 1][aim + 1];
+		dp[N][0] = 1;
+		for (int index = N - 1; index >= 0; index--) {
+			for (int rest = 0; rest <= aim; rest++) {
+				int ways = 0;
+				for (int zhang = 0; zhang * arr[index] <= rest; zhang++) {
+					ways += dp[index + 1][rest - (zhang * arr[index])];
+				}
+				dp[index][rest] = ways;
+			}
+		}
+		return dp[0][aim];
+	}
 }
