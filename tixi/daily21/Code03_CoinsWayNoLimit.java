ba7f3f51b876367e87429ef98f549d31ea62cbe5
gaotianpeng <gtpsydm@gmail.com>
2022-05-08 20:21:13 +0800

    daily practise

--- tixi/daily21/Code03_CoinsWayNoLimit.java
+++ tixi/daily21/Code03_CoinsWayNoLimit.java
@@ -45,4 +45,42 @@ public class Code03_CoinsWayNoLimit {
 		}
 		return dp[0][aim];
 	}
+
+    	public static int dp1(int[] arr, int aim) {
+		if (arr == null || arr.length == 0 || aim < 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int[][] dp = new int[N + 1][aim + 1];
+		dp[N][0] = 1;
+		for (int index = N - 1; index >= 0; index--) {
+			for (int rest = 0; rest <= aim; rest++) {
+				int ways = 0;
+				for (int zhang = 0; zhang * arr[index] <= rest; zhang++) {
+					ways += dp[index + 1][rest - (zhang * arr[index])];
+				}
+				dp[index][rest] = ways;
+			}
+		}
+		return dp[0][aim];
+	}
+
+	public static int dp2(int[] arr, int aim) {
+		if (arr == null || arr.length == 0 || aim < 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int[][] dp = new int[N + 1][aim + 1];
+		dp[N][0] = 1;
+		for (int index = N - 1; index >= 0; index--) {
+			for (int rest = 0; rest <= aim; rest++) {
+				dp[index][rest] = dp[index + 1][rest];
+				if (rest - arr[index] >= 0) {
+					dp[index][rest] += dp[index][rest - arr[index]];
+				}
+			}
+		}
+		return dp[0][aim];
+	}
+
 }
