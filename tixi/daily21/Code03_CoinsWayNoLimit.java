0e1ecc0ea79ecd5a117523b2f8c92a37cb8c7a47
gaotianpeng <gtpsydm@gmail.com>
2022-05-09 15:58:02 +0800

    daily practise

--- tixi/daily21/Code03_CoinsWayNoLimit.java
+++ tixi/daily21/Code03_CoinsWayNoLimit.java
@@ -83,4 +83,51 @@ public class Code03_CoinsWayNoLimit {
 		return dp[0][aim];
 	}
 
+	  	// 为了测试
+	public static int[] randomArray(int maxLen, int maxValue) {
+		int N = (int) (Math.random() * maxLen);
+		int[] arr = new int[N];
+		boolean[] has = new boolean[maxValue + 1];
+		for (int i = 0; i < N; i++) {
+			do {
+				arr[i] = (int) (Math.random() * maxValue) + 1;
+			} while (has[arr[i]]);
+			has[arr[i]] = true;
+		}
+		return arr;
+	}
+
+	// 为了测试
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	// 为了测试
+	public static void main(String[] args) {
+		int maxLen = 10;
+		int maxValue = 30;
+		int testTime = 1000000;
+		System.out.println("测试开始");
+		for (int i = 0; i < testTime; i++) {
+			int[] arr = randomArray(maxLen, maxValue);
+			int aim = (int) (Math.random() * maxValue);
+			int ans1 = coinsWay(arr, aim);
+			int ans2 = dp1(arr, aim);
+			int ans3 = dp2(arr, aim);
+			if (ans1 != ans2 || ans1 != ans3) {
+				System.out.println("Oops!");
+				printArray(arr);
+				System.out.println(aim);
+				System.out.println(ans1);
+				System.out.println(ans2);
+				System.out.println(ans3);
+				break;
+			}
+		}
+		System.out.println("测试结束");
+	}
+
 }
