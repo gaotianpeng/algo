97dcc629baf2cede1a787f48c55fdee98433935e
gaotianpeng <gtpsydm@gmail.com>
2023-02-25 10:50:01 +0800

    daily practise

--- leetcode/Code_0002_AddTwoNumbers.java
+++ leetcode/Code_0002_AddTwoNumbers.java
@@ -71,4 +71,44 @@ public class Code_0002_AddTwoNumbers {
 
         return prev;
     }
+
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = carry + val1 + val2;
+
+            if (head == null) {
+                head = new ListNode(sum % 10);
+                tail = head;
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(1);
+        }
+
+        return head;
+    }
 }
