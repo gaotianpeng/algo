846f3a79ea7a78f16fcbf8138ad30885909afeed
gaotianpeng <gtpsydm@gmail.com>
2023-03-19 09:38:56 +0800

    daily practise

--- tixi/daily16/Code06_TopologySort.java
+++ tixi/daily16/Code06_TopologySort.java
@@ -6,9 +6,10 @@ import java.util.List;
 import java.util.Queue;
 
 /*
-    1）在图中找到所有入度为0的点输出
-    2）把所有入度为0的点在图中删掉，继续找入度为0的点输出，周而复始
-    3）图的所有点都被删除后，依次输出的顺序就是拓扑排序
+    拓扑排序
+        1）在图中找到所有入度为0的点输出
+        2）把所有入度为0的点在图中删掉，继续找入度为0的点输出，周而复始
+        3）图的所有点都被删除后，依次输出的顺序就是拓扑排序
 
     要求：有向图且其中没有环
     应用：事件安排、编译顺序
@@ -20,23 +21,23 @@ public class Code06_TopologySort {
             return null;
         }
 
-        List<Node> ans = new LinkedList<>();
-        Queue<Node> zero_queue = new LinkedList<>();
         HashMap<Node, Integer> in_map = new HashMap<>();
-        for (Node node: graph.nodes.values()) {
-            in_map.put(node, node.in);
-            if (node.in == 0) {
-                zero_queue.add(node);
+        Queue<Node> zero_in_queue = new LinkedList<>();
+        for (Node cur: graph.nodes.values()) {
+            in_map.put(cur, cur.in);
+            if (cur.in == 0) {
+                zero_in_queue.add(cur);
             }
         }
 
-        while (!zero_queue.isEmpty()) {
-            Node cur = zero_queue.poll();
-            ans.add(cur);
-            for (Node next: cur.nexts) {
+        List<Node> ans = new LinkedList<>();
+        while (!zero_in_queue.isEmpty()) {
+            Node res = zero_in_queue.poll();
+            ans.add(res);
+            for (Node next: res.nexts) {
                 in_map.put(next, in_map.get(next) - 1);
                 if (in_map.get(next) == 0) {
-                    zero_queue.add(next);
+                    ans.add(next);
                 }
             }
         }
