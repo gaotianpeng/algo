6ac9d15e4d8e03035f628b833c96f3347108499b
gaotianpeng <gtpsydm@gmail.com>
2023-03-19 18:33:07 +0800

    daily practise

--- lintcode/MinimumSpanningTree.java
+++ lintcode/MinimumSpanningTree.java
@@ -3,6 +3,79 @@ package lintcode;
 import java.util.*;
 
 public class MinimumSpanningTree {
+        /**
+         * @param connections given a list of connections include two cities and cost
+         * @return a list of connections from results
+         */
+        // 比较函数
+        static Comparator<Connection> cmp = new Comparator<Connection>() {
+            public int compare(Connection a, Connection b) {
+                if (a.cost != b.cost) {
+                    return a.cost - b.cost;
+                }
+                if (a.city1.equals(b.city1)) {
+                    return a.city2.compareTo(b.city2);
+                }
+                return a.city1.compareTo(b.city1);
+            }
+        };
+
+        // 寻找根节点
+        static int Find(int x, int[] pre) {
+            // 若x为根结点则返回x，否则继续递归寻找根结点,同时进行路径压缩
+            if (x == pre[x]){
+                return x;
+            }
+            else{
+                return pre[x] = Find(pre[x], pre);
+            }
+        }
+
+        public List<Connection> lowestCost(List<Connection> connections) {
+            // 按边的权值大小进行升序排序
+            Collections.sort(connections, cmp);
+            List<Connection> mst = new ArrayList();
+            int n = connections.size();
+            // 用一个哈希表记录某城市对应的序号
+            HashMap<String, Integer> mp = new HashMap<>();
+            int cnt = 0;
+            // 处理所有的不同城市，每个城市对应一个编号
+            for (int i = 0; i < n; i++) {
+                String u = connections.get(i).city1;
+                String v = connections.get(i).city2;
+                if (!mp.containsKey(u)) {
+                    mp.put(u,cnt++);
+                }
+                if (!mp.containsKey(v)) {
+                    mp.put(v,cnt++);
+                }
+            }
+            int[] pre = new int[cnt];
+            // 初始化
+            for (int i = 0; i < cnt; i++) {
+                pre[i] = i;
+            }
+            for (int i = 0; i < n; i++) {
+                String u = connections.get(i).city1;
+                String v = connections.get(i).city2;
+                int numU = mp.get(u), numV = mp.get(v);
+                // 判断两端点是否连通
+                if (Find(numU, pre) == Find(numV, pre)) {
+                    continue;
+                }
+                mst.add(connections.get(i));
+                pre[pre[numU]] = pre[numV];
+            }
+            // 判断是否全部连通
+            int root = Find(0, pre);
+            for (int i = 1; i < cnt; i++) {
+                if (Find(i, pre) != root) {
+                    return new ArrayList<Connection>();
+                }
+            }
+            return mst;
+    }
+
     public static class Connection {
       public String city1, city2;
       public int cost;
@@ -12,63 +85,6 @@ public class MinimumSpanningTree {
           this.cost = cost;
       }
     }
-
-    /**
-        @param connections given a list of connections include two cities and cost
-        @return a list of connections from results
-     */
-    public List<Connection> lowestCost(List<Connection> connections) {
-        // Write your code here
-        Collections.sort(connections, comp);
-        Map<String, Integer> hash = new HashMap<String, Integer>();
-        int n = 0;
-        for (Connection connection : connections) {
-            if (!hash.containsKey(connection.city1)) {
-                hash.put(connection.city1, ++n);
-            }
-            if (!hash.containsKey(connection.city2)) {
-                hash.put(connection.city2, ++n);
-            }
-        }
-
-        int[] father = new int[n + 1];
-
-        List<Connection> results = new ArrayList<Connection>();
-        for (Connection connection : connections) {
-            int num1 = hash.get(connection.city1);
-            int num2 = hash.get(connection.city2);
-
-            int root1 = find(num1, father);
-            int root2 = find(num2, father);
-            if (root1 != root2) {
-                father[root1] = root2;
-                results.add(connection);
-            }
-        }
-
-        if (results.size() != n - 1)
-            return new ArrayList<Connection>();
-        return results;
-    }
-
-    static Comparator<Connection> comp = new Comparator<Connection>() {
-        public int compare(Connection a, Connection b) {
-            if (a.cost != b.cost)
-                return a.cost - b.cost;
-            if (a.city1.equals(b.city1)) {
-                return a.city2.compareTo(b.city2);
-            }
-            return a.city1.compareTo(b.city1);
-        }
-    };
-
-    public static  int find(int num, int[] father) {
-        if (father[num] == 0)
-            return num;
-
-        return father[num] = find(father[num], father);
-    }
-
     // version: 高频题班
     /**
         @param connections given a list of connections include two cities and cost
