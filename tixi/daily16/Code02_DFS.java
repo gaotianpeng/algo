2d27ba9c97dcd114f3273db5965049aef159acc0
gaotianpeng <gtpsydm@gmail.com>
2023-03-19 08:41:46 +0800

    daily practise

--- tixi/daily16/Code02_DFS.java
+++ tixi/daily16/Code02_DFS.java
@@ -1,27 +1,29 @@
 package tixi.daily16;
 
+import javax.swing.plaf.IconUIResource;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Stack;
 
 public class Code02_DFS {
-    static void dfs(Node node) {
-        if (node == null) {
+    static void dfs(Node start) {
+        if (start == null) {
             return;
         }
 
         Stack<Node> stack = new Stack<>();
         HashSet<Node> set = new HashSet<>();
-        stack.push(node);
-        set.add(node);
-        System.out.println(node.value);
+        stack.push(start);
+        set.add(start);
+        System.out.println(start.value);
         while (!stack.isEmpty()) {
             Node cur = stack.pop();
-            for (Node next : cur.nexts) {
+            for (Node next: cur.nexts) {
                 if (!set.contains(next)) {
                     stack.push(cur);
                     stack.push(next);
                     set.add(next);
-                    System.out.println(next.value);
+                    System.out.println(cur.value);
                     break;
                 }
             }
