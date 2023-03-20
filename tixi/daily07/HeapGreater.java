a719bade6e34407e57264418b5751d93f2be83a9
gaotianpeng <gtpsydm@gmail.com>
2023-03-20 22:49:09 +0800

    daily practise

--- tixi/daily07/HeapGreater.java
+++ tixi/daily07/HeapGreater.java
@@ -1,22 +1,23 @@
 package tixi.daily07;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.rmi.StubNotFoundException;
+import java.sql.Array;
+import java.sql.Struct;
+import java.util.*;
 
+// 小根堆
 public class HeapGreater<T> {
-
     private ArrayList<T> heap_;
     private HashMap<T, Integer> index_map_;
     private int heap_size_;
     private Comparator<? super T> comp_;
 
     public HeapGreater(Comparator<T> c) {
-       heap_ = new ArrayList<>();
-       index_map_ = new HashMap<>();
-       heap_size_ = 0;
-       comp_ = c;
+        heap_ = new ArrayList<>();
+        index_map_ = new HashMap<>();
+        heap_size_ = 0;
+        comp_ = c;
+
     }
 
     public boolean isEmpty() {
@@ -47,7 +48,6 @@ public class HeapGreater<T> {
         index_map_.remove(ans);
         heap_.remove(--heap_size_);
         heapify(0);
-
         return ans;
     }
 
@@ -73,21 +73,21 @@ public class HeapGreater<T> {
         for (T c: heap_) {
             ans.add(c);
         }
+
         return ans;
     }
 
     private void heapInsert(int index) {
-        while (comp_.compare(heap_.get(index), heap_.get((index - 1) / 2)) < 0) {
+        while (comp_.compare(heap_.get(index), heap_.get((index-1)/2)) < 0) {
             swap(index, (index - 1) / 2);
-            index = (index - 1) / 2;
+            index = (index - 1)/2;
         }
     }
 
     private void heapify(int index) {
         int left = index * 2 + 1;
         while (left < heap_size_) {
-            int best = left + 1 < heap_size_ && comp_.compare(
-                    heap_.get(left + 1), heap_.get(left)) < 0 ?(left + 1) : left;
+            int best = left + 1 < heap_size_ && comp_.compare(heap_.get(left+1), heap_.get(left)) < 0 ? left + 1 : left;
             best = comp_.compare(heap_.get(best), heap_.get(index)) < 0 ? best : index;
             if (best == index) {
                 break;
@@ -103,7 +103,63 @@ public class HeapGreater<T> {
         T o2 = heap_.get(j);
         heap_.set(i, o2);
         heap_.set(j, o1);
-        index_map_.put(o2, i);
         index_map_.put(o1, j);
+        index_map_.put(o2, i);
+    }
+
+
+    public static class Student {
+        public int classNo;
+        public int age;
+        public int id;
+
+        public Student(int c, int a, int i) {
+            classNo = c;
+            age = a;
+            id = i;
+        }
+    }
+
+    public static class StudentComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.age - o2.age;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        PriorityQueue<Student> priority_queue = new PriorityQueue<>(new StudentComparator());
+        Student s1 = null;
+        Student s2 = null;
+        Student s3 = null;
+        Student s4 = null;
+        Student s5 = null;
+
+        s1 = new Student(2, 50, 1111);
+        s2 = new Student(1, 60, 2222);
+        s3 = new Student(6, 15, 3333);
+        s4 = new Student(3, 17, 4444);
+        s5 = new Student(7, 28, 5555);
+
+
+
+
+        HeapGreater<Student>  greater = new HeapGreater<>(new StudentComparator());
+        greater.push(s1);
+        greater.push(s2);
+        greater.push(s3);
+        greater.push(s4);
+        greater.push(s5);
+
+        System.out.println(greater.peek().age);
+
+        s1.age = 5;
+        greater.resign(s1);
+
+        System.out.println(greater.peek().age);
+
     }
 }
