efadf01e27fed3039bdaf620be4012d887556e25
gaotianpeng <gtpsydm@gmail.com>
2023-03-25 17:22:06 +0800

    daily practise

---
+++ leetcode/Code_1438_LongestSubarrayLessThanLimit.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+public class Code_1438_LongestSubarrayLessThanLimit {
+    public int longestSubarray(int[] nums, int limit) {
+        if (nums == null || nums.length == 0 || limit < 0) {
+            return 0;
+        }
+
+        int ans = 0;
+        int n = nums.length;
+
+        for (int left = 0; left < n; ++left) {
+            for (int right = left; right < n; ++right) {
+                int max = nums[left];
+                int min = nums[left];
+                for (int i = left + 1; i <= right; ++i) {
+                    max = Math.max(max, nums[i]);
+                    min = Math.min(min, nums[i]);
+                }
+                if (max - min <= limit) {
+                    ans = Math.max(ans, right - left + 1);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public int longestSubarray1(int[] arr, int sum) {
+        if (arr == null || arr.length == 0 || sum < 0) {
+            return 0;
+        }
+
+        int n = arr.length;
+        int count = 0;
+        LinkedList<Integer> max_win = new LinkedList<>();
+        LinkedList<Integer> min_win = new LinkedList<>();
+        int right = 0;
+        for (int left = 0; left < n; ++left) {
+            while (right < n) {
+                while (!max_win.isEmpty() && arr[max_win.peekLast()] <= arr[right]) {
+                    max_win.pollLast();
+                }
+                max_win.addLast(right);
+                while (!min_win.isEmpty() && arr[min_win.peekLast()] >= arr[right]) {
+                    min_win.pollLast();
+                }
+                min_win.addLast(right);
+                if (arr[max_win.peekFirst()] - arr[min_win.peekFirst()] > sum) {
+                    break;
+                } else {
+                    right++;
+                }
+            }
+
+            count = Math.max(count, right - left);
+            if (max_win.peekFirst() == left) {
+                max_win.pollFirst();
+            }
+            if (min_win.peekFirst() == left) {
+                min_win.pollFirst();
+            }
+        }
+
+        return count;
+    }
+}
--- tixi/daily24/Code02_AllLessNumSubArray.java
+++ tixi/daily24/Code02_AllLessNumSubArray.java
@@ -8,44 +8,50 @@ public class Code02_AllLessNumSubArray {
         某个arr中的子数组sub，如果想达标，必须满足：
         sub中最大值 – sub中最小值 <= sum，
         返回arr中达标子数组的数量
+
+        leetcode 相关题目
+        1438. 绝对差不超过限制的最长连续子数组
+        https://leetcode.cn/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
      */
     public static int num(int[] arr, int sum) {
         if (arr == null || arr.length == 0 || sum < 0) {
             return 0;
         }
 
-        int n = arr.length;
-        int count = 0;
-        LinkedList<Integer> max_win = new LinkedList<>();
-        LinkedList<Integer> min_win = new LinkedList<>();
+        int ans = 0;
         int right = 0;
+        int n = arr.length;
+        LinkedList<Integer> qmax = new LinkedList<>();
+        LinkedList<Integer> qmin = new LinkedList<>();
         for (int left = 0; left < n; ++left) {
             while (right < n) {
-                while (!max_win.isEmpty() && arr[max_win.peekLast()] <= arr[right]) {
-                    max_win.pollLast();
+                while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[right]) {
+                    qmax.pollLast();
                 }
-                max_win.addLast(right);
-                while (!min_win.isEmpty() && arr[min_win.peekLast()] >= arr[right]) {
-                    min_win.pollLast();
+                qmax.addLast(right);
+
+                while (!qmin.isEmpty() && arr[qmin.peekLast()] >= arr[right]) {
+                    qmin.pollLast();
                 }
-                min_win.addLast(right);
-                if (arr[max_win.peekFirst()] - arr[min_win.peekFirst()] > sum) {
+                qmin.addLast(right);
+
+                if (arr[qmax.peekFirst()] - arr[qmin.peekFirst()] > sum) {
                     break;
                 } else {
-                    right++;
+                    ++right;
                 }
             }
 
-            count += right - left;
-            if (max_win.peekFirst() == left) {
-                max_win.pollFirst();
+            ans += right - left;
+            if (qmax.peekFirst() == left) {
+                qmax.pollFirst();
             }
-            if (min_win.peekFirst() == left) {
-                min_win.pollFirst();
+            if (qmin.peekFirst() == left) {
+                qmin.pollFirst();
             }
         }
 
-        return count;
+        return ans;
     }
 
     public static int test(int[] arr, int sum) {
