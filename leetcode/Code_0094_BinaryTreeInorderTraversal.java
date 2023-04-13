32d248c145b99e288d6f5c20e03c277fdba8c7b2
gaotianpeng <gtpsydm@gmail.com>
2023-04-13 23:20:03 +0800

    daily practise

--- leetcode/Code_0094_BinaryTreeInorderTraversal.java
+++ leetcode/Code_0094_BinaryTreeInorderTraversal.java
@@ -62,5 +62,33 @@ public class Code_0094_BinaryTreeInorderTraversal {
         return ans;
     }
 
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
 
+        TreeNode cur = root;
+        TreeNode most_right = null;
+        while (cur != null) {
+            most_right = cur.left;
+            if (most_right != null) {
+                while (most_right.right != null && most_right.right != cur) {
+                    most_right = most_right.right;
+                }
+
+                if (most_right.right == null) {
+                    most_right.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    most_right.right = null;
+                }
+            }
+            ans.add(cur.val);
+            cur = cur.right;
+        }
+
+        return ans;
+    }
 }
