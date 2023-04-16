5f1ba4ff60479f357bae926448f98b6afb6e470d
gaotianpeng <gtpsydm@gmail.com>
2023-04-16 16:08:24 +0800

    daily practise

--- tixi/daily32/Code03_AC.java
+++ tixi/daily32/Code03_AC.java
@@ -1,5 +1,10 @@
 package tixi.daily32;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
     解决在一个大字符串中，找到多个候选字符串的问题
     核心算法
@@ -9,4 +14,109 @@ package tixi.daily32;
         拥有最大的匹配长度。fail指针就指向那个字符串的最后一个字符所对应的节点
  */
 public class Code03_AC {
+    public static class Node {
+        public String end;
+        public boolean end_use;
+        public Node fail;
+        public Node[] nexts;
+
+        public Node() {
+            end_use = false;
+            end = null;
+            fail = null;
+            nexts = new Node[26];
+        }
+    }
+
+    public static class ACAutomation {
+        private Node root;
+
+        public ACAutomation() {
+            root = new Node();
+        }
+
+        public void insert(String s) {
+            char[] str = s.toCharArray();
+            Node cur = root;
+            int index = 0;
+            for (int i = 0; i < str.length; ++i) {
+                index = str[i] - 'a';
+                if (cur.nexts[index] == null) {
+                    cur.nexts[index] = new Node();
+                }
+                cur = cur.nexts[index];
+            }
+            cur.end = s;
+        }
+
+        public void build() {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            Node cur = null;
+            Node cfail = null;
+
+            while (!queue.isEmpty()) {
+                cur = queue.poll();
+                for (int i = 0; i < 26; ++i) {
+                    if (cur.nexts[i] != null) {
+                        cur.nexts[i].fail = root;
+                        cfail = cur.fail;
+                        while (cfail != null) {
+                            if (cfail.nexts[i] != null) {
+                                cur.nexts[i].fail = cfail.nexts[i];
+                                break;
+                            }
+                            cfail = cfail.fail;
+                        }
+                        queue.add(cur.nexts[i]);
+                    }
+                }
+            }
+        }
+
+        public List<String> containWords(String content) {
+            char[] str = content.toCharArray();
+            Node cur = root;
+            Node follow = null;
+            int index = 0;
+            List<String> ans = new ArrayList<>();
+            for (int i = 0; i < str.length; ++i) {
+                index = str[i] - 'a';
+                while (cur.nexts[index] == null && cur != root) {
+                    cur = cur.fail;
+                }
+                cur = cur.nexts[index] != null ? cur.nexts[index] : root;
+                follow = cur;
+                while (follow != root) {
+                    if (follow.end_use) {
+                        break;
+                    }
+                    if (follow.end != null) {
+                        ans.add(follow.end);
+                        follow.end_use = true;
+                    }
+                    follow = follow.fail;
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test start...");
+        ACAutomation ac = new ACAutomation();
+        ac.insert("dhe");
+        ac.insert("he");
+        ac.insert("abcdheks");
+        ac.insert("aaaaaa");
+        // 设置fail指针
+        ac.build();
+
+        List<String> contains = ac.containWords("abcdhekskdjfafhasldkflskdjhwqaeruv");
+        for (String word : contains) {
+            System.out.println(word);
+        }
+
+        System.out.println("test end");
+    }
 }
