db3fddf1d1ee08bd3d2fca8fa4e6a3455e86e925
gaotianpeng <gtpsydm@gmail.com>
2023-04-16 18:15:05 +0800

    daily practise

--- tixi/daily32/Code02_IndexTree2D.java
+++ tixi/daily32/Code02_IndexTree2D.java
@@ -1,5 +1,17 @@
 package tixi.daily32;
 
+/*
+    https://leetcode.cn/problems/range-sum-query-2d-mutable/
+    leetcode 308 二维区域和检索 - 可变
+        给你一个二维矩阵 matrix ，处理以下类型的多个查询:
+        更新 matrix 中单元格的值。
+        计算由左上角 (row1, col1) 和右下角 (row2, col2) 定义的 matrix内矩阵元素的和。
+        实现 NumMatrix 类：
+    NumMatrix(int[][] matrix) 用整数矩阵matrix 初始化对象。
+    void update(int row, int col, int val) 更新 matrix[row][col] 的值到 val 。
+    int sumRegion(int row1, int col1, int row2, int col2) 返回矩阵matrix 中指定矩形区域元素的 和 ，该区域由 左上角 (row1, col1)
+     和 右下角 (row2, col2) 界定
+ */
 public class Code02_IndexTree2D {
     public static class NumMatrix {
         private int[][] tree;
