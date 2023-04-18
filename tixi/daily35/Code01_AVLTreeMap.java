6abcc03a2a9a49f0159873366d7c4bcfa928ee1b
gaotianpeng <gtpsydm@gmail.com>
2023-04-18 22:25:12 +0800

    daily practise

--- tixi/daily35/Code01_AVLTreeMap.java
+++ tixi/daily35/Code01_AVLTreeMap.java
@@ -46,11 +46,18 @@ public class Code01_AVLTreeMap {
                 lastNode.val = value;
             } else {
                 ++size;
-            
+                root = add(root, key, value);
             }
         }
 
         public void remove(K key) {
+            if (key == null) {
+                return;
+            }
+            if (containsKey(key)) {
+                size--;
+                root = delete(root, key);
+            }
         }
 
         // 获取key 对应的 value
@@ -76,7 +83,7 @@ public class Code01_AVLTreeMap {
             while (ans.l != null) {
                 ans = ans.l;
             }
-            return null;
+            return ans.key;
         }
 
         // 获取最大的key
@@ -103,9 +110,16 @@ public class Code01_AVLTreeMap {
             return lastNoBigNode == null ? null : lastNoBigNode.key;
         }
 
+        // 返回大于等于给定键的最小键
         public K ceilingKey(K key) {
-            return null;
+            if (key == null) {
+                return null;
+            }
+
+            AVLNode<K, V> last_no_big = findLastNoBigIndex(key);
+            return last_no_big == null ? null : last_no_big.key;
         }
+
         /*
                    A       右旋         B
                   /  \               /    \
@@ -142,7 +156,7 @@ public class Code01_AVLTreeMap {
             return right;
         }
 
-        // 找到 <= key 最近的
+        // 找到 <= key 于给定键值的最大节点，返回该节点
         private AVLNode<K, V> findLastIndex(K key) {
             AVLNode<K, V> pre = root;
             AVLNode<K, V> cur = root;
@@ -159,7 +173,7 @@ public class Code01_AVLTreeMap {
             return pre;
         }
 
-        // 找到 >=key 的最近的
+        // 用于查找 >=key的最小节点并返回该节点
         private AVLNode<K, V> findLastNoSmallIndex(K key) {
             AVLNode<K, V> ans = null;
             AVLNode<K, V> cur = root;
@@ -195,10 +209,127 @@ public class Code01_AVLTreeMap {
             }
             return ans;
         }
+
+        private AVLNode<K, V> delete(AVLNode<K, V> cur, K key) {
+            if (key.compareTo(cur.key) > 0) {
+                cur.r = delete(cur.r, key);
+            } else if (key.compareTo(cur.key) < 0) {
+                cur.l = delete(cur.l, key);
+            } else {
+                if (cur.l == null && cur.r == null) {
+                    cur = null;
+                } else if (cur.l == null && cur.r != null) {
+                    cur = cur.r;
+                } else if (cur.l != null && cur.r == null) {
+                    cur = cur.l;
+                } else {
+                    AVLNode<K, V> des = cur.r;
+                    while (des.l != null) {
+                        des = des.l;
+                    }
+                    cur.r = delete(cur.r, des.key);
+                    des.l = cur.l;
+                    des.r = cur.r;
+                    cur = des;
+                }
+            }
+
+            if (cur != null) {
+                cur.height = Math.max(cur.l != null ? cur.l.height : 0, cur.r != null ? cur.r.height : 0) + 1;
+            }
+
+            return maintain(cur);
+        }
+
+        /*
+
+         */
+        private AVLNode<K, V> maintain(AVLNode<K, V> cur) {
+            if (cur == null) {
+                return null;
+            }
+
+            int left_height = cur.l != null ? cur.l.height : 0;
+            int right_height = cur.r != null ? cur.r.height : 0;
+            if (Math.abs(left_height - right_height) > 1) {
+                // 左子树比右子树高，需要进行右旋操作
+                /*
+                           A(cur)  右旋         B
+                          /  \               /    \
+                        B     T             C      A
+                       / \                 / \    /  \
+                      C   K               S   F   K   T
+                     / \
+                    S   F
+                 */
+                if (left_height > right_height) {
+                    int left_left_height = cur.l != null && cur.l.l != null ? cur.l.l.height : 0;
+                    int left_right_height = cur.l != null && cur.l.r != null ? cur.l.r.height : 0;
+                    if (left_left_height >= left_right_height) {
+                        cur = rightRotate(cur); // cur.l.l.height >= cur.l.r.height 对cur右旋
+                    } else {
+                        /*
+                                   A(cur)  对cur.l左旋    A(cur) 对cur右旋    K
+                                  /  \                 /  \               /   \
+                                B     T               K    T             B     A
+                               / \                   / \                / \   / \
+                              C   K                 B   F              C   S  F  T
+                                 / \               / \
+                                S   F             C   S
+                         */
+                        cur.l = leftRotate(cur.l); // cur.l.l.height < cur.l.r.height 先对cur.l 进行左旋，再对cur进程右旋
+                        cur = rightRotate(cur);
+                    }
+                } else { // 右子树比左子树高，需要进行左旋操作
+                    /*
+                         A(cur)      左旋         B
+                       /   \                   /   \
+                      T     B                 A     C
+                          /   \              / \   /  \
+                         K     C            T   K  S  F
+                              /  \
+                             S   F
+                    */
+                    int right_left_height = cur.r != null && cur.r.l != null ? cur.r.l.height : 0;
+                    int right_right_height = cur.r != null && cur.r.r != null  ? cur.r.r.height : 0;
+                    if (right_right_height > right_left_height) {
+                        cur = leftRotate(cur);
+                    } else {
+                        cur.r = leftRotate(cur.r);
+                        cur = leftRotate(cur);
+                    }
+                }
+            }
+
+            return cur;
+        }
+
+        private AVLNode<K, V> add(AVLNode<K, V> cur, K key, V value) {
+            if (cur == null) {
+                return new AVLNode<K, V>(key, value);
+            } else {
+                if (key.compareTo(cur.key) < 0) {
+                    cur.l = add(cur.l, key, value);
+                } else {
+                    cur.r = add(cur.r, key, value);
+                }
+
+                cur.height = Math.max(cur.l != null ? cur.l.height : 0, cur.r != null ? cur.r.height : 0) + 1;
+                return maintain(cur);
+            }
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("test start...");
+        AVLTree<Integer, Integer> tree = new AVLTree<>();
+        tree.put(1, 2);
+        tree.put(2, 3);
+        System.out.println(tree.size());
+        System.out.println(tree.get(1));
+        System.out.println(tree.get(2));
+        tree.remove(2);
+        System.out.println(tree.size());
         System.out.println("test end");
     }
 }
