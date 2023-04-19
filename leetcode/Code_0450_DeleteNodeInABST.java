cdbe2fa82b74d60f5ccee3d66b509e9ca853a496
gaotianpeng <gtpsydm@gmail.com>
2023-04-19 23:02:20 +0800

    daily practise

--- leetcode/Code_0450_DeleteNodeInABST.java
+++ leetcode/Code_0450_DeleteNodeInABST.java
@@ -14,7 +14,7 @@ public class Code_0450_DeleteNodeInABST {
         }
     }
 
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode deleteNode1(TreeNode root, int key) {
         if (root == null) {
             return null;
         }
@@ -38,13 +38,13 @@ public class Code_0450_DeleteNodeInABST {
         // 1) cur 左树为空，右树也为空
         if (cur.left == null && cur.right == null) {
             cur = null;
-            // 2) cur 右树为空
+        // 2) cur 右树为空
         } else if (cur.right == null) {
             cur = cur.left;
-            // 3) cur 左树为空
+        // 3) cur 左树为空
         } else if (cur.left == null) {
             cur = cur.right;
-            // 4) cur 左树、右树均不为空，用右树最小节点代替cur
+        // 4) cur 左树、右树均不为空，用右树最小节点代替cur
         } else {
             TreeNode right_left_most = cur.right, right_left_most_p = cur;
             while (right_left_most.left != null) {
@@ -70,6 +70,63 @@ public class Code_0450_DeleteNodeInABST {
             } else {
                 cur_parent.right = cur;
             }
+            return root;
+        }
+    }
+
+    public TreeNode deleteNode2(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode cur = root;
+        TreeNode cur_parent = null;
+        while (cur != null && cur.val != key) {
+            cur_parent = cur;
+            if (cur.val > key) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        // 未找到要删除的节点，直接返回root
+        if (cur == null) {
+            return root;
+        }
+
+        if (cur.left == null && cur.right == null) {
+            cur = null;
+        } else if (cur.left == null && cur.right != null) {
+            cur = cur.right;
+        } else if (cur.left != null && cur.right == null) {
+            cur = cur.left;
+        } else {
+            TreeNode left_most_right = cur.left;
+            TreeNode left_most_right_parent = cur;
+            while (left_most_right.right != null) {
+                left_most_right_parent = left_most_right;
+                left_most_right = left_most_right.right;
+            }
+
+            if (left_most_right_parent.val == cur.val) {
+                left_most_right_parent.left = left_most_right.left;
+            } else {
+                left_most_right_parent.right = left_most_right.left;
+            }
+            left_most_right.left = cur.left;
+            left_most_right.right = cur.right;
+            cur = left_most_right;
+        }
+
+        if (cur_parent == null) {
+            return cur;
+        } else {
+            if (cur_parent.left != null && cur_parent.left.val == key) {
+                cur_parent.left = cur;
+            } else {
+                cur_parent.right = cur;
+            }
+
             return root;
         }
     }
