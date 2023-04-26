6167c8b27b55c35ac0b37947511281b8c866998a
gaotianpeng <gtpsydm@gmail.com>
2023-04-26 23:41:32 +0800

    daily practise

--- tixi/daily36/Code01_SizeBalancedTreeMap.java
+++ tixi/daily36/Code01_SizeBalancedTreeMap.java
@@ -11,31 +11,120 @@ public class Code01_SizeBalancedTreeMap {
         public int size;
 
         public SBTNode(K k, V v) {
+            size = 1;
             key = k;
             val = v;
-            size = 1;
         }
     }
 
     public static class SizeBalancedTreeMap<K extends Comparable<K>, V> {
         private SBTNode<K, V> root;
 
-        private SBTNode<K, V> rightRotate(SBTNode<K, V> cur) {
-            SBTNode<K, V> left = cur.l;
-            cur.l = left.r;
-            left.r = cur;
-            left.size = cur.size;
-            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
-            return left;
+        public SizeBalancedTreeMap() {
         }
 
-        private SBTNode<K, V> leftRotate(SBTNode<K, V> cur) {
-            SBTNode<K, V> right = cur.r;
-            cur.r = right.l;
-            right.l = cur;
-            right.size = cur.size;
-            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
-            return right;
+        public boolean containsKey(K key) {
+            if (key == null) {
+                return false;
+            }
+
+            SBTNode<K, V> last_node = findLastIndex(key);
+            return last_node != null && key.compareTo(last_node.key) == 0 ? true : false;
+        }
+
+        public int size() {
+            return root == null ? 0 : root.size;
+        }
+
+        public void put(K key, V val) {
+            if (key == null) {
+                return;
+            }
+
+            SBTNode<K, V> last_node = findLastIndex(key);
+            if (last_node != null && key.compareTo(last_node.key) == 0) {
+                last_node.val = val;
+            } else {
+                root = add(root, key, val);
+            }
+        }
+
+        public V get(K key) {
+            if (key == null) {
+                return null;
+            }
+
+            SBTNode<K, V> last_node = findLastIndex(key);
+            if (last_node != null && key.compareTo(last_node.key) == 0) {
+                return last_node.val;
+            } else {
+                return null;
+            }
+        }
+
+        public void remove(K key) {
+            if (key == null) {
+                return;
+            }
+
+            if (containsKey(key)) {
+                root = delete(root, key);
+            }
+        }
+
+        private SBTNode<K, V> delete(SBTNode<K, V> cur, K key) {
+            if (cur == null) {
+                return null;
+            }
+            cur.size--;
+
+            if (key.compareTo(cur.key) > 0) {
+                cur.r = delete(cur.r, key);
+            } else if (key.compareTo(cur.key) < 0) {
+                cur.l = delete(cur.l, key);
+            } else { // 当前要删除cur
+                if (cur.l == null && cur.r == null) {
+                    cur = null;
+                } else if (cur.l != null && cur.r == null) {
+                    cur = cur.l;
+                } else if (cur.l == null && cur.r != null) {
+                    cur = cur.r;
+                } else {
+                    SBTNode<K, V> pre = null;
+                    SBTNode<K, V> des = cur.r;
+                    des.size--;
+                    while (des.l != null) {
+                        pre = des;
+                        des = des.l;
+                        des.size--;
+                    }
+
+                    if (pre != null) {
+                        pre.l = des.r;
+                        des.r = cur.r;
+                    }
+                    des.l = cur.l;
+                    des.size = des.l.size + (des.r == null ? 0 : des.r.size) + 1;
+                    cur = des;
+                }
+            }
+
+            return cur;
+        }
+
+
+        private SBTNode<K, V> add(SBTNode<K, V> cur, K key, V val) {
+            if (cur == null) {
+                return new SBTNode<>(key, val);
+            } else {
+                ++cur.size;
+                if (key.compareTo(cur.key) < 0) {
+                    cur.l = add(cur.l, key, val);
+                } else {
+                    cur.r = add(cur.r, key, val);
+                }
+                return maintain(cur);
+            }
         }
 
         private SBTNode<K, V> maintain(SBTNode<K, V> cur) {
@@ -74,12 +163,35 @@ public class Code01_SizeBalancedTreeMap {
             return cur;
         }
 
+        private SBTNode<K, V> rightRotate(SBTNode<K, V> cur) {
+            SBTNode<K, V> left = cur.l;
+            cur.l = left.r;
+            left.r = cur;
+            left.size = cur.size;
+            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
+            return left;
+        }
+
+        private SBTNode<K, V> leftRotate(SBTNode<K, V> cur) {
+            SBTNode<K, V> right = cur.r;
+            cur.r = right.l;
+            right.l = cur;
+            right.size = cur.size;
+            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
+            return right;
+        }
+
+
         // 找到最后一个 <= key
         private SBTNode<K, V> findLastIndex(K key) {
-            SBTNode<K, V> pre = null;
+            if (key == null) {
+                return null;
+            }
+
+            SBTNode<K, V> ans = root;
             SBTNode<K, V> cur = root;
             while (cur != null) {
-                pre = cur;
+                ans = cur;
                 if (key.compareTo(cur.key) == 0) {
                     break;
                 } else if (key.compareTo(cur.key) < 0) {
@@ -89,152 +201,7 @@ public class Code01_SizeBalancedTreeMap {
                 }
             }
 
-            return pre;
-        }
-
-        // >= key
-        private SBTNode<K, V> findLastNoSmallIndex(K key) {
-            SBTNode<K, V> ans = null;
-            SBTNode<K, V> cur = root;
-            while (cur != null) {
-              if (key.compareTo(cur.key) == 0) {
-                  ans = cur;
-                  break;
-              } else if (key.compareTo(cur.key) < 0) {
-                  ans = cur;
-                  cur = cur.l;
-              } else {
-                  cur = cur.r;
-              }
-            }
-
             return ans;
-        }
-
-        // <= key
-        private SBTNode<K, V> findLastNoBigIndex(K key) {
-            SBTNode<K, V> ans = null;
-            SBTNode<K, V> cur = root;
-            while (cur != null) {
-                if (key.compareTo(cur.key) == 0) {
-                    ans = cur;
-                    break;
-                } else if (key.compareTo(cur.key) < 0) {
-                    cur = cur.l;
-                } else {
-                    ans = cur;
-                    cur = cur.r;
-                }
-            }
-
-            return ans;
-        }
-
-        private SBTNode<K, V> getIndex(SBTNode<K, V> cur, int kth) {
-            if (kth == (cur.l != null ? cur.l.size : 0) + 1) {
-                return cur;
-            } else if (kth <= (cur.l != null ? cur.l.size : 0)) {
-                return getIndex(cur.l, kth);
-            } else {
-                return getIndex(cur.r, kth - ((cur.l != null) ? cur.l.size : 0) - 1);
-            }
-        }
-
-        public int size() {
-            return root == null ? 0 : root.size;
-        }
-
-        public boolean containsKey(K key) {
-            if (key == null) {
-                return false;
-            }
-
-            SBTNode<K, V> lastNode = findLastIndex(key);
-            return lastNode != null && key.compareTo(lastNode.key) == 0 ? true : false;
-        }
-
-        public void put(K key, V value) {
-            if (key == null) {
-                return;
-            }
-
-            SBTNode<K, V> lastNode = findLastIndex(key);
-            if (lastNode != null && key.compareTo(lastNode.key) == 0) {
-                lastNode.val = value;
-            } else {
-                root = add(root, key, value);
-            }
-        }
-
-        public void remove(K key) {
-            if (key == null) {
-                return;
-            }
-            if (containsKey(key)) {
-                root = delete(root, key);
-            }
-        }
-
-        public V get(K key) {
-            if (key == null) {
-                return null;
-            }
-
-            SBTNode<K, V> lastNode = findLastIndex(key);
-            if (lastNode != null && key.compareTo(lastNode.key) == 0) {
-                return lastNode.val;
-            } else {
-                return null;
-            }
-        }
-        private SBTNode<K, V> add(SBTNode<K, V> cur, K key, V value) {
-            if (cur == null) {
-                return new SBTNode<K, V>(key, value);
-            } else {
-                cur.size++;
-                if (key.compareTo(cur.key) < 0) {
-                    cur.l = add(cur.l, key, value);
-                } else {
-                    cur.r = add(cur.r, key, value);
-                }
-                return maintain(cur);
-            }
-        }
-
-        private SBTNode<K, V> delete(SBTNode<K, V> cur, K key) {
-            cur.size--;
-            if (key.compareTo(cur.key) > 0) {
-                cur.r = delete(cur.r, key);
-            } else if (key.compareTo(cur.key) < 0) {
-                cur.l = delete(cur.l, key);
-            } else {
-                if (cur.l == null && cur.r == null) {
-                    cur = null;
-                } else if (cur.l == null && cur.r != null) {
-                    cur = cur.r;
-                } else if (cur.l != null && cur.r == null) {
-                    cur = cur.l;
-                } else {
-                    SBTNode<K, V> pre = null;
-                    SBTNode<K, V> des = cur.r;
-                    des.size--;
-                    while (des.l != null) {
-                        pre = des;
-                        des = des.l;
-                        des.size--;
-                    }
-
-                    if (pre != null) {
-                        pre.l = des.r;
-                        des.r = cur.r;
-                    }
-                    des.l = cur.l;
-                    des.size = des.l.size + (des.r == null ? 0 : des.r.size) + 1;
-                    cur = des;
-                }
-            }
-
-            return cur;
         }
 
         public K firstKey() {
@@ -246,6 +213,7 @@ public class Code01_SizeBalancedTreeMap {
             while (cur.l != null) {
                 cur = cur.l;
             }
+
             return cur.key;
         }
 
@@ -253,12 +221,65 @@ public class Code01_SizeBalancedTreeMap {
             if (root == null) {
                 return null;
             }
+
             SBTNode<K, V> cur = root;
             while (cur.r != null) {
                 cur = cur.r;
             }
-
             return cur.key;
+        }
+
+        // >= key
+        private SBTNode<K, V> findNoSmallNodeInde(K key) {
+            if (key == null) {
+                 return null;
+            }
+
+            SBTNode<K, V> ans = null;
+            SBTNode<K, V> cur = root;
+            while (cur != null){
+                if (key.compareTo(cur.key) == 0) {
+                    ans = cur;
+                    break;
+                } else if (key.compareTo(cur.key) < 0) {
+                    ans = cur;
+                    cur = cur.l;
+                } else {
+                    cur = cur.r;
+                }
+            }
+
+            return ans;
+        }
+        // <= key
+        private SBTNode<K, V> findNoBigNodeInde(K key) {
+            if (key == null) {
+                 return null;
+            }
+
+            SBTNode<K, V> ans = null;
+            SBTNode<K, V> cur = root;
+            while (cur != null){
+                if (key.compareTo(cur.key) == 0) {
+                    ans = cur;
+                    break;
+                } else if (key.compareTo(cur.key) < 0) {
+                    cur = cur.l;
+                } else {
+                    ans = cur;
+                    cur = cur.r;
+                }
+            }
+
+            return ans;
+        }
+
+        public K ceilingKey(K key) {
+            if (key == null) {
+                return null;
+            }
+            SBTNode<K, V> no_small = findNoSmallNodeInde(key);
+            return no_small != null ? no_small.key : null;
         }
 
         public K floorKey(K key) {
@@ -266,17 +287,8 @@ public class Code01_SizeBalancedTreeMap {
                 return null;
             }
 
-            SBTNode<K, V> lastNoBigNode = findLastNoBigIndex(key);
-            return lastNoBigNode == null ? null : lastNoBigNode.key;
-        }
-
-        public K ceilingKey(K key) {
-            if (key == null) {
-                return null;
-            }
-
-            SBTNode<K, V> lastNoSmallNode = findLastNoSmallIndex(key);
-            return lastNoSmallNode == null ? null : lastNoSmallNode.key;
+            SBTNode<K, V> no_big = findNoBigNodeInde(key);
+            return no_big != null ? no_big.key : null;
         }
     }
 
@@ -307,7 +319,7 @@ public class Code01_SizeBalancedTreeMap {
                         break;
                     }
                 } else if (Math.random() < 0.4) {
-                    int key = (int)(Math.random() * max_val);
+                    int key = (int) (Math.random() * max_val);
                     if (tree_map.get(key) == sb_tree.get(key)) {
                         continue;
                     } else {
@@ -315,48 +327,49 @@ public class Code01_SizeBalancedTreeMap {
                         System.out.println("test faield 3");
                         break;
                     }
-                } else if (Math.random() < 0.6) {
-                    int key = (int)(Math.random() * max_val);
-                    tree_map.remove(key);
-                    sb_tree.remove(key);
-                    if (tree_map.size() != sb_tree.size()) {
-                        success = false;
-                        System.out.println("test failed 4");
-                        break;
-                    }
-                } else if (Math.random() < 0.7) {
-                    if (tree_map.isEmpty()) {
-                        continue;
-                    }
-                    if (tree_map.firstKey() != sb_tree.firstKey()) {
-                        success = false;
-                        System.out.println("test failed 5");
-                        break;
-                    }
-                } else if (Math.random() < 0.8) {
-                    if (tree_map.isEmpty()) {
-                        continue;
-                    }
-                    if (tree_map.lastKey() != sb_tree.lastKey()) {
-                        success = false;
-                        System.out.println("test failed 6");
-                        break;
-                    }
-                } else if (Math.random() < 0.9) {
-                    int key = (int)(Math.random() * max_val);
-                    if (tree_map.ceilingKey(key) != tree_map.ceilingKey(key)) {
-                        success = false;
-                        System.out.println("test failed 7");
-                        break;
-                    }
-                } else {
-                    int key = (int)(Math.random() * max_val);
-                    if (tree_map.floorKey(key) != tree_map.floorKey(key)) {
-                        success = false;
-                        System.out.println("test failed 7");
-                        break;
-                    }
                 }
+//                } else if (Math.random() < 0.6) {
+//                    int key = (int)(Math.random() * max_val);
+//                    tree_map.remove(key);
+//                    sb_tree.remove(key);
+//                    if (tree_map.size() != sb_tree.size()) {
+//                        success = false;
+//                        System.out.println("test failed 4");
+//                        break;
+//                    }
+//                } else if (Math.random() < 0.7) {
+//                    if (tree_map.isEmpty()) {
+//                        continue;
+//                    }
+//                    if (tree_map.firstKey() != sb_tree.firstKey()) {
+//                        success = false;
+//                        System.out.println("test failed 5");
+//                        break;
+//                    }
+//                } else if (Math.random() < 0.8) {
+//                    if (tree_map.isEmpty()) {
+//                        continue;
+//                    }
+//                    if (tree_map.lastKey() != sb_tree.lastKey()) {
+//                        success = false;
+//                        System.out.println("test failed 6");
+//                        break;
+//                    }
+//                } else if (Math.random() < 0.9) {
+//                    int key = (int)(Math.random() * max_val);
+//                    if (tree_map.ceilingKey(key) != tree_map.ceilingKey(key)) {
+//                        success = false;
+//                        System.out.println("test failed 7");
+//                        break;
+//                    }
+//                } else {
+//                    int key = (int)(Math.random() * max_val);
+//                    if (tree_map.floorKey(key) != tree_map.floorKey(key)) {
+//                        success = false;
+//                        System.out.println("test failed 7");
+//                        break;
+//                    }
+//                }
             }
             if (!success) {
                 break;
