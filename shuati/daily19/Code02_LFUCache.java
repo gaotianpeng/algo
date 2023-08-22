35955c8fb6821be3920059ccb29bc4c7e3cd8bdd
gaotianpeng <gtpsydm@gmail.com>
2023-08-22 21:07:45 +0800

    daily practise

---
+++ shuati/daily19/Code02_LFUCache.java
@@ -0,0 +1,35 @@
+package shuati.daily19;
+
+import java.util.HashMap;
+
+public class Code02_LFUCache {
+    public static class LFUCache {
+        public LFUCache(int capacity) {
+        }
+
+        public int get(int key) {
+            return 0;
+        }
+
+        public void put(int key, int value) {
+        }
+    }
+
+    public static class LFUCacheTest {
+        public LFUCacheTest(int capacity) {
+            capacity_ = capacity;
+        }
+
+        public int get(int key) {
+            return 0;
+        }
+
+        public void put(int key, int value) {
+        }
+
+        private int capacity_;
+    }
+
+    public static void main(String[] args) {
+    }
+}
