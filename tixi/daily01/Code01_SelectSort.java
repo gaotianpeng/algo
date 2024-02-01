d4cd3cdaff581ca64ce16c089cd381223094db60
gaotianpeng <gtpsydm@gmail.com>
2024-02-01 16:11:13 +0800

    daily practise

--- tixi/daily01/Code01_SelectSort.java
+++ tixi/daily01/Code01_SelectSort.java
@@ -17,15 +17,16 @@ public class Code01_SelectSort {
             return;
         }
 
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) {
-            int min_index = i;
-            for (int j = i + 1; j < n; j++) {
-                min_index = arr[j] < arr[min_index] ? j: min_index;
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
             }
 
-            if (min_index != i) {
-                swap(arr, i, min_index);
+            if (min_idx != i) {
+                swap(arr, min_idx, i);
             }
         }
     }
