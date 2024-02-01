69c299e3cf9c136fb6799fb589476628441a6066
gaotianpeng <gtpsydm@gmail.com>
2024-02-01 16:28:00 +0800

    daily practise

--- tixi/daily01/Code02_BubbleSort.java
+++ tixi/daily01/Code02_BubbleSort.java
@@ -17,8 +17,7 @@ public class Code02_BubbleSort {
             return;
         }
 
-        int n = arr.length;
-        for (int i = n - 1; i > 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j+1]) {
                     swap(arr, j, j + 1);
