fdb328b99d2de6ff568a5e7c7bfb605c209f8b31
gaotianpeng <gtpsydm@gmail.com>
2024-03-05 22:23:16 +0800

    daily practise

--- leetcode/Code_0452_MinimumNumberOfArrowsToBurstBalloons.java
+++ leetcode/Code_0452_MinimumNumberOfArrowsToBurstBalloons.java
@@ -11,15 +11,22 @@ public class Code_0452_MinimumNumberOfArrowsToBurstBalloons {
             return 0;
         }
 
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
-        int pos = points[0][1];
+        int[][] copyPoints = new int[points.length][2];
+        for (int i = 0; i < copyPoints.length; ++i) {
+            copyPoints[i][0] = points[i][0];
+            copyPoints[i][1] = points[i][1];
+        }
+
+        Arrays.sort(copyPoints, (a, b) -> Integer.compare(a[1], b[1]));
+        int pos = copyPoints[0][1];
         int ans = 1;
-        for (int[] balloon: points) {
-            if (balloon[0] > pos) {
-                pos = balloon[1];
+        for (int[] point: copyPoints) {
+            if (point[0] > pos) {
+                pos = point[1];
                 ++ans;
             }
         }
+
         return ans;
     }
 }
