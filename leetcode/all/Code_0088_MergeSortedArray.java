6b2a64e71f3bf7c2c28abd17684d329e276b50d4
gaotianpenng <gtpsydm@gmail.com>
2024-08-04 13:24:58 +0800

    daily practise

--- leetcode/all/Code_0088_MergeSortedArray.java
+++ leetcode/all/Code_0088_MergeSortedArray.java
@@ -1,24 +1,163 @@
 package leetcode.all;
 
+
+import java.util.Arrays;
+
 public class Code_0088_MergeSortedArray {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    /*
+    88. 合并两个有序数组
+    给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+    请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+    注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，
+    其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
+    示例 1：
+    输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    输出：[1,2,2,3,5,6]
+    解释：需要合并 [1,2,3] 和 [2,5,6] 。
+    合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+
+    示例 2：
+    输入：nums1 = [1], m = 1, nums2 = [], n = 0
+    输出：[1]
+    解释：需要合并 [1] 和 [] 。
+    合并结果是 [1] 。
+
+    示例 3：
+    输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+    输出：[1]
+    解释：需要合并的数组是 [] 和 [1] 。
+    合并结果是 [1] 。
+    注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+
+
+    提示：
+    nums1.length == m + n
+    nums2.length == n
+    0 <= m, n <= 200
+    1 <= m + n <= 200
+    -109 <= nums1[i], nums2[j] <= 109
+
+    进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m - 1;
         int p2 = n - 1;
-        int tail = m + n - 1;
+        int p = m + n -1;
         while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[tail--]  = nums1[p1--];
-            } else {
-                nums1[tail--]  = nums2[p2--];
-            }
+            nums1[p--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
         }
 
         while (p1 >= 0) {
-            nums1[tail--] = nums1[p1--];
+            nums1[p--] = nums1[p1--];
         }
 
         while (p2 >= 0) {
-            nums1[tail--] = nums2[p2--];
+            nums1[p--] = nums2[p2--];
         }
+    }
+
+    /*
+        for test
+     */
+    public static void test(int[] nums1, int m, int[] nums2, int n) {
+        if (n < 1) {
+            return;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            nums1[i + m] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+
+        int[] ret = new int[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+            ret[i] = arr[i];
+        }
+
+        return ret;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test start...");
+        int testTimes = 100000;
+        int maxVal = 50;
+        int maxLen = 10;
+        boolean success = true;
+
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr = generateRandomArray(maxLen, maxVal);
+            int[] nums2 = generateRandomArray(maxLen, maxVal);
+            int m = arr.length;
+            int n = nums2.length;
+            Arrays.sort(arr);
+            Arrays.sort(nums2);
+            int[] nums1 = new int[arr.length + nums2.length];
+            for (int j = 0; j < arr.length; ++j) {
+                nums1[j] = arr[j];
+            }
+
+            int[] copyNums1 = copyArray(nums1);
+            int[] copyNums2 = copyArray(nums2);
+            merge(nums1, m, nums2, n);
+            test(copyNums1, m, copyNums2, n);
+            if (!isEqual(nums1, copyNums1)) {
+                success = false;
+                break;
+            }
+        }
+
+        System.out.println(success ? "success": "failed");
+        System.out.println("test end");
     }
 }
