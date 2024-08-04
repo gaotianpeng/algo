281001a6b12587d64c4b6b4d3873bdf693f0ec20
gaotianpenng <gtpsydm@gmail.com>
2024-08-04 13:40:27 +0800

    daily practise

--- leetcode/top150/Code_0027_RemoveElement.java
+++ leetcode/top150/Code_0027_RemoveElement.java
@@ -18,4 +18,104 @@ public class Code_0027_RemoveElement {
         }
         return ans;
     }
+
+    public static int test(int[] nums, int  val) {
+        int[] tmp = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == val) {
+                continue;
+            }
+            tmp[index++] = nums[i];
+        }
+
+        for (int i = 0; i < index; ++i) {
+            nums[i] = tmp[i];
+        }
+        return index;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int)(maxValue * Math.random());
+        }
+
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+
+        int[] ret = new int[arr.length];
+        for (int i = 0; i < arr.length; ++i) {
+            ret[i] = arr[i];
+        }
+
+        return ret;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2, int n) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test start...");
+        int testTimes = 100000;
+        int maxVal = 50;
+        int maxLen = 10;
+        boolean success = true;
+
+        for (int i = 0; i < testTimes; i++) {
+            int[] nums1 = generateRandomArray(maxLen, maxVal);
+            int[] nums2 = copyArray(nums1);
+            int val  = (int)((maxVal + 1) * Math.random()) - (int)(maxVal * Math.random());
+            int ans1 = removeElement(nums1, val);
+            int ans2 = test(nums2, val);
+            if (ans1 != ans2) {
+                success = false;
+                break;
+            }
+
+            if (!isEqual(nums1, nums2, ans1)) {
+                success = false;
+                break;
+            }
+        }
+
+        System.out.println(success ? "success": "failed");
+        System.out.println("test end");
+    }
 }
